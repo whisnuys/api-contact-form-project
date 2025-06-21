b8191f17b0227175f82f545bd3ef9a9c9d98fbe4
whisnuys <whisnusaputra30@gmail.com>
2025-06-21 21:36:02 +0700

    cms laravel

---
+++ cms-contact-form/resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="bg-[#2A4D7D] text-white py-4 mt-auto">
+    <div class="container mx-auto text-center">
+        <p>
+            &copy; 2025 -
+            <a href="https://whisnuys.com" target="_blank" class="text-blue-400 hover:underline">
+                whisnuys
+            </a>
+        </p>
+    </div>
+</footer>
